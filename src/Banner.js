2d8ba1f4fb3ed64d8bf4efa20d2acb0a5c031189
Rakshith <rakshithhgd@gmail.com>
2021-10-06 21:33:31 +0530

    6-10

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "./axios";
+import requests from "./request";
 
 function Banner() {
-  return <header></header>;
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner_buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My list</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
